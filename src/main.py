1498defed733bc29f2020aff9f0e4becd55b7582
Alexandr Belov <aleksandr.b.dev-f@innofin.team>
2026-09-07 19:39:02 +0500

    feat: openapi

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from posts import routes as posts_routes
 
-app = FastAPI()
+app = FastAPI(
+    title="Posts Api",
+    description="Проект постов",
+    version="0.0.1",
+    openapi_tags=[{"name": "Posts", "description": "Управление постами"}],
+)
 app.include_router(posts_routes.router)
--- src/posts/routes.py
+++ src/posts/routes.py
@@ -1,23 +1,57 @@
 from fastapi import APIRouter, Depends
-from .schemas import PostPath, CreatePostRequest, UpdatePostRequest, UpdatePostResponse
+from .schemas import (
+    PostPath,
+    CreatePostRequest,
+    UpdatePostRequest,
+    UpdatePostResponse,
+    GetPostResponse,
+    CreatePostResponse,
+    DeletePostResponse,
+)
 
-router = APIRouter(prefix="/posts")
+router = APIRouter(prefix="/posts", tags=["Posts"])
 
-@router.get("/{post_id}")
+
+@router.get(
+    "/{post_id}",
+    response_model=GetPostResponse,
+    description="""
+    Получения id поста
+""",
+)
 def get_post(path: PostPath = Depends()):
-    return { "content": path.post_id}
+    return GetPostResponse(post_id=path.post_id)
 
-@router.post("/")
+
+@router.post(
+    "/",
+    response_model=CreatePostResponse,
+    status_code=201,
+    description="""
+    Создание поста
+""",
+)
 def create_post(data: CreatePostRequest):
     return data
 
-@router.patch("/{post_id}", response_model=UpdatePostResponse)
-def update_post(data: UpdatePostRequest, path: PostPath = Depends()):
-    return UpdatePostResponse(
-        id=path.post_id,
-        content=data.content
-    )
 
-@router.delete("/{post_id}")
+@router.patch(
+    "/{post_id}",
+    response_model=UpdatePostResponse,
+    description="""
+    Обновление поста
+""",
+)
+def update_post(data: UpdatePostRequest, path: PostPath = Depends()):
+    return UpdatePostResponse(id=path.post_id, content=data.content)
+
+
+@router.delete(
+    "/{post_id}",
+    response_model=DeletePostResponse,
+    description="""
+    Удаление поста
+""",
+)
 def delete_post(path: PostPath = Depends()):
-    return { "removed_post": path.post_id }
+    return DeletePostResponse(removed_post_id=path.post_id)
