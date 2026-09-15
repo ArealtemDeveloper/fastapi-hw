09c0d222a2fa9f5c8647ead069f96fa25e430430
Alexandr Belov <aleksandr.b.dev-f@innofin.team>
2026-09-15 16:26:09 +0500

    feat: add crud

--- src/posts/service.py
+++ src/posts/service.py
@@ -1,5 +1,8 @@
 from typing import Annotated
 from fastapi import Depends
+
+from posts.model import Post
+from posts.schemas import CreatePostRequest, GetAllPostsParams, UpdatePostRequest
 from .repository import PostRepository, PostRepositoryDeps
 
 
@@ -11,8 +14,51 @@ class PostService:
     def __init__(self, repo: PostRepository):
         self.repo = repo
 
-    def get_post_id(self, post_id: int):
-        return self.repo.get_by_id(post_id)
+    async def get_post_id(self, post_id: int):
+        return await self.repo.get_by_id(post_id)
+
+    async def get_all_posts_with_params(self, params: GetAllPostsParams):
+        return await self.repo.get_all(params.offset, params.limit)
+
+    async def create_post(self, data: CreatePostRequest):
+        post = Post(content=data.content)
+        await self.repo.save(post)
+
+        return post
+
+    async def update_post(self, data: UpdatePostRequest, post_id: int):
+        post = await self.get_post_id(post_id)
+
+        if post is None:
+            return None
+
+        patch = data.model_dump(exclude_unset=True)
+
+        for field, value in patch.items():
+            setattr(post, field, value)
+            await self.repo.save(post)
+
+        return post
+
+    async def delete_post(self, post_id: int):
+        post = await self.get_post_id(post_id)
+
+        if post is None:
+            return None
+
+        setattr(post, "is_deleted", True)
+        await self.repo.save(post)
+        return True
+
+    async def increase_post_likes(self, post_id: int):
+        post = await self.get_post_id(post_id)
+
+        if post is None:
+            return None
+
+        setattr(post, "likes_count", post.likes_count + 1)
+        await self.repo.save(post)
+        return post
 
 
 PostServiceDeps = Annotated[PostService, Depends(get_post_service)]
